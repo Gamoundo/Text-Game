6db1bcf11900774fcd81710d3ff9c54c2697abce
Gamoundo <akilgg@gmail.com>
2021-10-09 00:58:52 -0400

    init commit

---
+++ flixtacular/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Flixtacular</h1>
+      <h3> Start</h3>
+    </div>
+  );
+}
+
+export default App;
