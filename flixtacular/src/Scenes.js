eb703f762cc4f8c6793f539d6190f06cebdd5cee
Gamoundo <akilgg@gmail.com>
2021-10-16 16:43:32 -0400

    added two more scenes to the scenelist

--- flixtacular/src/Scenes.js
+++ flixtacular/src/Scenes.js
@@ -44,22 +44,20 @@ const sceneList = [
         "There didn't seem to be anything special about it at a glance.",
         "But, when have they steered you wrong.",
         "Although the movie list left a lot to be desired.",
-        "",
-        "",  
+          
       ],
     [
-        "",
-        "",
-        "",
-        "",
-        "",  
+        "They looked like basic movies, how could this be different?",
+        "Well, your friend can't be right all the time.",
+        `${props.uname}: I think I'll just stare at the wall`,
+        "???: why stare when you can watch something engaging? You may have seen these movies, but you've never experienced them.",
+        `Using that word experience again, is this really supposed to be different?`,  
       ],
       [
-        "",
-        "",
-        "",
-        "",
-        "",  
+        "This site was peddled hard even though there is not much in their collection",
+        "I'm bored and skeptical, I'm sure that's a fun combo.",
+        "Do I watch one of these or find something else to do?",
+         
       ],
 
 ]
@@ -70,7 +68,7 @@ const sceneList = [
             {props.uname && displayScene(sceneList[count])}
             
            {count !== 0 && <button onClick={countDown}> Prev </button>}
-           {count < sceneList.length && <button onClick={countUp}> Next</button>}
+           {count < sceneList.length -1 && <button onClick={countUp}> Next</button>}
         </div>
     )
 }
